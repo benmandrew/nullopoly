615665a089fb8e20084fc9964c401146ec2e3019
benmandrew <benmandrew@gmail.com>
2025-07-26 14:08:05 +0100

    Add more charge payment tests

--- tests/test_game.py
+++ tests/test_game.py
@@ -32,18 +32,29 @@ class TestPayments(unittest.TestCase):
             cards.PropertyCard("TestProperty", 2, cards.PropertyColour.RED)
         )
 
-    def test_charge_payment_with_enough_money(self):
+    def test_charge_payment_1(self):
         with patch("util.get_number_input", side_effect=[]):
             money, properties = self.g.charge_payment(self.player, 5)
             self.assertEqual(len(money), 1)
             self.assertEqual(len(properties), 0)
+            self.assertEqual(self.player.total_bank_value(), 3)
+            self.assertEqual(len(self.player.properties_to_list()), 2)
 
-    def test_charge_payment_with_insufficient_money(self):
+    def test_charge_payment_2(self):
         with patch("util.get_number_input", side_effect=[1]):
             money, properties = self.g.charge_payment(self.player, 10)
             self.assertEqual(len(money), 2)
             self.assertEqual(len(properties), 1)
+            self.assertEqual(self.player.total_bank_value(), 0)
             self.assertEqual(len(self.player.properties_to_list()), 1)
+
+    def test_charge_payment_3(self):
+        with patch("util.get_number_input", side_effect=[1, 1]):
+            money, properties = self.g.charge_payment(self.player, 20)
+            self.assertEqual(len(money), 2)
+            self.assertEqual(len(properties), 2)
+            self.assertEqual(self.player.total_bank_value(), 0)
+            self.assertEqual(len(self.player.properties_to_list()), 0)
 
 
 if __name__ == "__main__":
